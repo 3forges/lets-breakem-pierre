0f0653ecbb967b815d12c83bbf57ad1daad64f03
Jean-Baptiste Lasselle <jean.baptiste.lasselle@gmail.com>
2023-12-10 14:38:16 +0100

    adjusting networking

--- experiments/first/utils/shell/launch.sh
+++ experiments/first/utils/shell/launch.sh
@@ -7,7 +7,6 @@ mkdir -p $HOME/.github
 
 docker pull docker.redpanda.com/redpandadata/redpanda:v23.2.6
 docker pull docker.redpanda.com/redpandadata/console:v2.3.1
-docker pull docker.redpanda.com/redpandadata/redpanda:v23.2.6
 docker pull minio/minio:RELEASE.2023-08-29T23-07-35Z
 docker pull amazon/aws-cli
 docker pull mageai/mageai:latest
